ae0cabcc1f0269ca4d99abb331bbb362647c5baa
chenaski <mail2chenaski@gmail.com>
2021-08-05 14:29:09 +0300

    Fix `create-review-app` workflow

--- bin/create-db-copy.sh
+++ bin/create-db-copy.sh
@@ -7,4 +7,4 @@ export PGPASSWORD="$1"
 psql -U deploy -d postgres -c "DROP DATABASE IF EXISTS \"$2\";"
 psql -U deploy -d postgres -c "CREATE DATABASE \"$2\";"
 pg_dump -U deploy -d realworld | psql "$2"
-psql -U deploy -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$2\" to realworld;";
+psql -U deploy -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$2\" to deploy;";
