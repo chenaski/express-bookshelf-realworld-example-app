8490e8ca66f057752ce497ca0f873cf40381f9a0
chenaski <mail2chenaski@gmail.com>
2021-08-08 01:05:29 +0300

    Add `ecosystem.config.js`

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: process.env.DB_NAME,
+      script: 'lib/server.js',
+      watch: '.',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: process.env.SSH_USER,
+      host: process.env.SSH_HOST,
+      ref: `origin/${process.env.DEPLOY_BRANCH}`,
+      repo: process.env.REPOSITORY_URL,
+      path: process.env.DEPLOY_DEST_DIR,
+      ssh_options: ['StrictHostChecking=no'],
+      'post-deploy': `cp ../.env . && npm i && env DB_NAME=${process.env.DB_NAME} pm2 startOrRestart ecosystem.config.js --env production --update-env`,
+    },
+  },
+};
