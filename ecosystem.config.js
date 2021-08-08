9d2fb35594daa23d1bbc1da43bad70bff972c87d
chenaski <mail2chenaski@gmail.com>
2021-08-08 18:28:24 +0300

    Fix `ecosystem.config.js`

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,11 +13,11 @@ module.exports = {
     production: {
       user: process.env.SSH_USER,
       host: process.env.SSH_HOST,
-      ref: 'origin/main',
       repo: process.env.REPOSITORY_URL,
       path: process.env.DEPLOY_DEST_DIR,
       ssh_options: ['StrictHostKeyChecking=no'],
-      'post-deploy': `cp ../.env . && npm i && env DB_NAME=${process.env.DB_NAME} pm2 startOrRestart ecosystem.config.js --env production --update-env`,
+      fetch: '--all',
+      'post-deploy': `cp ../.env . && npm i && DB_NAME=${process.env.DB_NAME} pm2 startOrRestart ecosystem.config.js --env production --update-env`,
     },
   },
 };
